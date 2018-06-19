1d6be314c5dfe5f9227a598b62ee667c43b1366a
Tarik Sarac <tarik.sarac@symphony.is>
2018-06-19 23:51:59 +0200

    english strings

--- src/components/MainContent/MainContent.js
+++ src/components/MainContent/MainContent.js
@@ -2,7 +2,19 @@ import React from 'react'
 import FeaturedPicture from "../FeaturedPicture/FeaturedPicture";
 import Heading from "../Heading/Heading";
 import ProductsContainer from "../Product/ProductsContainer";
-import {about, heading, pliva, plivaText, products, temporaryImg, firstHeading, firstSubHeading, lastHeading, lastSubHeading} from "../../constants/constants";
+import {
+    about,
+    heading,
+    pliva,
+    plivaText,
+    products,
+    temporaryImg,
+    firstHeading,
+    firstSubHeading,
+    lastHeading,
+    lastSubHeading,
+    offer
+} from "../../constants/constants";
 
 const MainContent = () => {
 
@@ -11,7 +23,7 @@ const MainContent = () => {
             <FeaturedPicture  title={firstHeading} subTitle={firstSubHeading} picture={temporaryImg}/>
             <Heading heading={heading} line subHeading={about}/>
             <FeaturedPicture about={plivaText} picture={pliva}/>
-            <Heading heading={'2018'} line subHeading={'Ponuda Izleta za 2018. godinu'}/>
+            <Heading heading={'2018'} line subHeading={offer}/>
             <ProductsContainer products={products}/>
             <Heading heading={lastHeading} subHeading={lastSubHeading} line/>
         </div>
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,16 +1,24 @@
 export const firstHeading = 'BEST OF BOSNIA & HERZEGOVINA'
-export const firstSubHeading = 'Osjetite nevjerovatnu Bosnu i Hercegovinu'
-export const temporaryImg = require('../images/sebilj.jpg')
+export const firstSubHeading = 'Experience the incredible Bosnia and Herzegovina'
+
+export const heading = 'Take a break and relax'
+export const about = 'Visit the heart-shaped country, the place where the East meets the West, ' +
+    'the country with exceptionally rich gastronomy, magical natural beauty and warm-hearted people. ' +
+    'Enjoy the Bosnian coffee drinking ritual.'
+
+export const plivaText = 'In a very short period Bosnia and Herzegovina has become ' +
+    'a tourist fairy-tale. Thanks to the beauty of our country the tourist season every year ' +
+    'becomes richer in different trips on offer to our guests. We are ready to provide all the ' +
+    'necessary information about trips and transfers as well as to recommend the best service based on demands of our guests. '
+export const offer = 'In our offer we highlight the trips which represent the best and must-see destinations in Bosnia and Herzegovina'
+
+export const lastHeading = 'Experience the incredible Bosnia and Herzegovina'
+export const lastSubHeading = 'Our agency’s goal is to help you see and experience the beauty' +
+    ' of Bosnia and Herzegovina in the best way possible. ' +
+    'We thank everyone who decides to put their trust in us!'
+
+    export const temporaryImg = require('../images/sebilj.jpg')
 export const pliva = require('../images/pliva.jpg')
-export const plivaText = 'U veoma kratkom period Bosna i Hercegovina je postala turistička bajka. Zahvaljujući ljepotama naše zemlje iz\n' +
-    'godine u godinu, turistička sezona je bogatija ponudom izleta koji se mogu ponuditi našim gostima. Mi kao\n' +
-    'kompanija smo spremni pružiti sve potrebne informacije vezane za izlete i transfere, kao i preporučiti uslugu na\n' +
-    'temelju želja naših gostiju.\n' +
-    'Prema našem mišljenju, za naše goste smo izabrali izlete koje smtramo najkvalitetnijim u odnosu na cijenu.'
-export const about = 'Posjetite zemlju u obliku srca, mjesto gdje se spajaju istok i zapad, zemlju sa bogatom\n' +
-    '                gastronomskom ponudom, čarobnim prirodnim ljepotama, toplim ljudima.\n' +
-    '                Uživajte u bosanskom ritualu ispijanja kafa.'
-export const heading = 'Odmorite dušu'
 export const sarajevo = 'https://drscdn.500px.org/photo/72791701/q%3D80_m%3D2000/v2?webp=true&sig=ab909fa759b56af27e3840eb1bac889f6c17a9644b62a90e95a511f7a370a13b'
 export const jajce = 'https://drscdn.500px.org/photo/199890883/q%3D80_m%3D1500/v2?webp=true&sig=554a08724fe8f124cb1ca07b1bd696d0bde5d6de1d886bfeae7d27d02ce5c303'
 export const mostar = 'https://drscdn.500px.org/photo/100843871/q%3D80_m%3D2000/v2?webp=true&sig=acd845aa07fdca3789e7a1af7f4e70a78e07fc7ae9042cea206970105cbf958f'
@@ -20,9 +28,7 @@ export const products = [{name:'Sarajevo-Jajce-Mostar', images:[sarajevo, mostar
     {name:'Sarajevo-Jajce', images:[sarajevo, jajce], description:'4 noćenja, 5 dana', price:'100$'},
     {name:'Sarajevo-Mostar', images:[sarajevo, mostar], description:'4 noćenja, 5 dana', price:'100$'}]
 
-export const lastHeading = 'doživite ljepote Bosne i Hercegovine'
-export const lastSubHeading = 'Cilj naše agencije jeste da vam pomognemo da upoznate i doživite ljepote Bosne i Hercegovine na najbolji mogući\n' +
-    '\n' +
-    'način.\n' +
-    '\n' +
-    'Zahvaljujemo se svima koji odluče svoje povjerenje prepustiti nama!'
+
+const tour = [
+    {}
+]
