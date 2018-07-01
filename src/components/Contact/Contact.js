94eb65e2e9f081f1b98534cef482af9ab246d793
Tarik Sarac <tarik.sarac@symphony.is>
2018-07-01 15:39:02 +0200

    test contact form

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -7,12 +7,19 @@ const Contact = () => {
             <div className={'Contact'}>
                 <div className={'contact-heading'}>CONTACT US</div>
                 <div className={'contact-detail'}>info@gobook.ba &nbsp; &nbsp; &nbsp; &nbsp;/ &nbsp; &nbsp; &nbsp; &nbsp;poznatog junaka 23, Sarajevo&nbsp;&nbsp;&nbsp;&nbsp; /&nbsp; &nbsp;&nbsp;&nbsp; +3876111111</div>
-                <form action="https://formspree.io/tarik.sarac@gmail.com"
-                      method="POST">
-                    <input id={'name'} placeholder={'Name'} type={'text'} name={'Name'}/>
+                {/*<form action="https://formspree.io/tarik.sarac@gmail.com"*/}
+                      {/*method="POST">*/}
+                    {/*<input id={'name'} placeholder={'Name'} type={'text'} name={'Name'}/>*/}
+                    {/*<input id={'name'} placeholder={'Email'} type={'email'} name={'_replyto'}/>*/}
+                    {/*<input placeholder={'Subject'} type={'text'} name={'Subject'}/>*/}
+                    {/*<textarea placeholder={'Message'} name={'Message'}/>*/}
+                    {/*<input type="submit" value="Send"/>*/}
+                {/*</form> */}
+                <form name="contact" method="POST" netlify>
+                    <input id={'name'} placeholder={'Name'} type={'text'} name={'name'}/>
                     <input id={'name'} placeholder={'Email'} type={'email'} name={'_replyto'}/>
-                    <input placeholder={'Subject'} type={'text'} name={'Subject'}/>
-                    <textarea placeholder={'Message'} name={'Message'}/>
+                    <input placeholder={'Subject'} type={'text'} name={'subject'}/>
+                    <textarea placeholder={'Message'} name={'message'}/>
                     <input type="submit" value="Send"/>
                 </form>
             </div>
