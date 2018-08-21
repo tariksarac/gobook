bcfb568c9c2751156205e4447f08008926fb8d56
Tarik Sarac <tarik.sarac@symphony.is>
2018-08-21 21:09:13 +0200

    fix build error npm install --save-dev @babel/runtime

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,7 +9,7 @@ const Footer = (props) => {
     return (
         <div className={'Footer'}>
             <div className={'footer-contact'}>
-                <span>📞 Viber, WhatsApp 👉 +387 61025015</span><span>gobook@gobook.ba</span>{' '}
+                <span><span role="img">📞 </span>Viber, WhatsApp <span role="img">👉</span>+387 61025015</span><span>gobook@gobook.ba</span>
             </div>
             <div className={'footer-logo'}>
                 <img src={logo} alt={'Go Book'} onClick={() => props.history.push('/')}/>
